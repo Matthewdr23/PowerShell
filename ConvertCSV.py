8c7a2d476916a63f5a461374a413647b4e2365d1
Matthew Velez <emoskull3@gmail.com>
2024-06-15 09:57:48 -0400

    changes and explaination added to ConvertCSV script

--- ConvertCSV.py
+++ ConvertCSV.py
@@ -5,6 +5,11 @@ from xlsxwriter.workbook import Workbook
 from openpyxl import Workbook
 import pandas as pd
 
+'''
+The purpose of this scriptn is to change a directory of CSV files in xlsx files for easier use of pasrsing data in other scripts.
+The reason for it's creation is because SailPoint allows the users to download reports but only as a CSV file. This became an issue when trying to automate 
+the Testing process with attestation as data in a CSV file does not correlate with data in a XLSX file so the conversion made things easier.
+'''
 
 def csv_conversion():
     for csvfile in glob.glob(os.path.join('.', '*.csv')):
